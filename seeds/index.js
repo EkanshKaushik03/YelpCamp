a7bc7089d83e7deb7daf196c0cb285e5cb26dc23
Ekansh Kaushik <ekanshkaushik321@gmail.com>
2024-12-23 20:08:52 +0530

    Basic Authorization

--- seeds/index.js
+++ seeds/index.js
@@ -20,6 +20,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000)
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6767f97aca9971b9d6d20e92',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://picsum.photos/1920/1080',
