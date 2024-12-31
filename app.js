57790669cc54b90a4892b24da29a05958ee72ade
Ekansh Kaushik <ekanshkaushik321@gmail.com>
2024-12-31 23:03:26 +0530

    Ready to Deploy

--- app.js
+++ app.js
@@ -15,15 +15,18 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet')
+const MongoStore = require('connect-mongo');
 
 
 
 const userRoutes = require('./routes/users');
 const campgroundsRoutes = require('./routes/campground');
 const reviewsRoutes = require('./routes/reviews');
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+mongoose.connect(dbUrl)
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -42,9 +45,25 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(mongoSanitize());
 
+
+const secret = process.env.SECRET || 'thisshouldbeabettersecret!';
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    touchAfter: 24 * 60 * 60,
+    crypto: {
+        secret
+    }
+});
+
+store.on("error", function(e) {
+    console.log("session store error",e)
+})
+
 const sessionConfig = {
+    store,
     name: 'session',
-    secret: 'thisshouldbeabettersecret!',
+    secret,
     resave: false,
     saveUninitialized: true,
     cookie: {
