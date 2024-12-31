9a586ff461a6d7f89ce9f1e333040f391eb0b9b9
Ekansh Kaushik <ekanshkaushik321@gmail.com>
2025-01-01 02:03:49 +0530

    fixed map zoom

--- public/Javascripts/clusterMap.js
+++ public/Javascripts/clusterMap.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([47.0202,-113.1331], 3);
+const map = L.map('map').setView([47.0202,-113.1331], 1);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
